495a6257bc291eec868a94bd958a5b0d98cb4480
Matthias Strubel <matthias.strubel@aod-rpg.de>
2024-11-07 22:14:37 +0100

    Improve error message for missing config file in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,14 @@ CONFIG_FILE="/app/config/batcontrol_config.yaml"
 
 # Check if the config file is mounted
 if [ ! -f "$CONFIG_FILE" ]; then
-  echo "Config file not found: $CONFIG_FILE"
+  echo "ERROR: Config file not found: $CONFIG_FILE !"
+  echo "       Please mount the config file to /app/config/batcontrol_config.yaml"
+  echo "       You can download a sample config file from :"
+  if [ "snapshot" == "$BATCONTROL_VERSION" ]; then
+     echo "        https://raw.githubusercontent.com/muexxl/batcontrol/${BATCONTROL_GIT_SHA}/config/batcontrol_config_dummy.yaml"
+  else
+     echo "        https://raw.githubusercontent.com/muexxl/batcontrol/refs/tags/${BATCONTROL_VERSION}/config/batcontrol_config_dummy.yaml"
+  fi
   exit 1
 fi
 
